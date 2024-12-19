5ec48d62f1f23170a44888879d23931f43dfdacd
aneesa04 <aneesafatima238@gmail.com>
2024-12-19 20:24:20 +0530

    adding all config changes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,8 +6,8 @@ import '../../global.css'
 const index = () => {
   return (
     <SafeAreaView>
-      <View className="bg-red-300" >
-        <Text className="text-red-600">ColorMind Home page!!</Text>
+      <View className="bg-red-300 h-20" >
+        <Text className="text-red-600">ColorMind Home BYE BYE page!!</Text>
 
       </View>
     </SafeAreaView>
