900d0e7b55a202da51e0577aade03fe834cf0a86
aneesa04 <aneesafatima238@gmail.com>
2025-04-14 21:05:54 +0530

    feat: added a new image processing query

---
+++ query/features/imageApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import Constants from "expo-constants";
+export const imageApi = createApi({
+  reducerPath: "imageApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${Constants.expoConfig?.extra?.EXPO_BACKEND_URL}api/v1/design-studio`,
+  }),
+  endpoints: (builder) => ({
+    maskedImage: builder.query({
+      query: (imgURL: string) => {
+        return {
+          url: "/create-item-mask",
+          method: "GET",
+          params: {
+            imgURL,
+          }, 
+        };
+      },
+    }),
+  }),
+});
+
+export const { useMaskedImageQuery } = imageApi;
